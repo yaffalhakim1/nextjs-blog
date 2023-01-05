eeeac9adeb03d75062e80e70ea4c3fbba8d293c2
yaffalhakim1 <yafi.tamfan08@gmail.com>
2023-01-05 13:37:21 +0700

    init

--- pages/detailpost.tsx
+++ pages/detailpost.tsx
@@ -14,7 +14,7 @@ export default function DetailPost() {
         <div className="md:w-6/12 w-full mx-auto flex items-center flex-col">
           <PostMetaTitle
             category="UI Design"
-            date=" 6 april 2022 "
+            date=" 6 april 2022"
             title="Understanding color theory: the color wheel and finding complementary colors"
             center
           />
@@ -30,24 +30,26 @@ export default function DetailPost() {
         <div className="md:w-8/12 w-full mx-auto leading-relaxed">
           <p className="text-xl mb-4">
             Male sixth sea it a. Brought was signs female darkness signs form
-            cattle land grass whose from subdue also they're their brought seas
-            isn't, to day from bearing grass third midst after beginning man
-            which you're. Dry, gathering beginning given made them evening.
+            cattle land grass whose from subdue also they`&apos;`re their
+            brought seas isn`&apos;`t, to day from bearing grass third midst
+            after beginning man which you`&apos;`re. Dry, gathering beginning
+            given made them evening.
           </p>
-          <p className="mb-4 ">
+          <p className="mb-4">
             Lights dry. Thing, likeness, forth shall replenish upon abundantly
             our green. Seed green sea that lesser divided creature beginning
             land him signs stars give firmament gathered. Wherein there their
-            morning a he grass. Don't made moving for them bring creature us
-            you'll tree second deep good unto good may. Us yielding. Have. Man
-            upon set multiply moved from under seasons abundantly earth brought
-            a. They're open moved years saw isn't morning darkness. Over,
-            waters, every let wherein great were fifth saw was lights very our
-            place won't and him Third fourth moving him whales behold. Beast
-            second stars lights great was don't green give subdue his. Third
-            given made created, they're forth god replenish have whales first
-            can't light was. Herb you'll them beast kind divided. Were beginning
-            fly air multiply god Yielding sea don't were forth.
+            morning a he grass. Don`&apos;`t made moving for them bring creature
+            us you`&apos;`ll tree second deep good unto good may. Us yielding.
+            Have. Man upon set multiply moved from under seasons abundantly
+            earth brought a. They`&apos;`re open moved years saw isn`&apos;`t
+            morning darkness. Over, waters, every let wherein great were fifth
+            saw was lights very our place won`&apos;`t and him Third fourth
+            moving him whales behold. Beast second stars lights great was
+            don`&apos;`t green give subdue his. Third given made created,
+            they`&apos;`re forth god replenish have whales first can`&apos;`t
+            light was. Herb you`&apos;`ll them beast kind divided. Were
+            beginning fly air multiply god Yielding sea don`&apos;`t were forth.
           </p>
         </div>
       </Container>
